863753a378ab44f6be9a7729a5c18955b008c8a5
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-04-05 21:10:52 -0500

    finished readme

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ work day scheduler app
 
 ## Description
 
-This program asks the user how long they want their password to be (min 8 - max 128) and if they would like to include lowercase letters, uppercase letters, numbers, or special characters in their password. The user must select at least 1 character type (lowercase, uppercase, special character, number). They must answer either "yes" or "no" to all prompts about character type or 8-18 for the password length prompt, if they fail to do any if this, they will be sent to the beginning of the program to start over. A password is randomly generated based off the user criteria.
+This app has 24 time blocks, one for every hour of the day. In each hour block you can enter text to describe what task(s) you need to do at that particular time of the day. You can save your tasks by clicking the floppy disk icon and come back to the page whenever you need to and your tasks will still be there.The grey boxes indicate tasks that are past due, the red box is the task that you need to do at the moment, and the green boxes are tasks that you'll need to do in the future. At the top of the page there is displayed the current date and time, which you can reference to if needed.
 
 ## Technology Used
 
@@ -35,33 +35,32 @@ This program asks the user how long they want their password to be (min 8 - max 
 
 ## Usage
 
-1. Click red "Generate Password".
-2. Enter number between 8-128.
-3. Enter "yes" or "no" to having lowercase letters included.
-4. Enter "yes" or "no" to having uppercase letters included.
-5. Enter "yes" or "no" to having special characters included.
-6. Your password will show up in the text box!
+1. Choose a time block to put your task in, use mouse to click the text input area and type in your to-dos
+2. Click the save buton, which is a floppy disk icon to save your to-do
 
-![Alt Text](./assests/gifs/Password-App-Demo.gif)
+![Alt Text](./assests/gifs/site-usage.gif)
 
 ## Code Highlight
 
-I wanted to highlight this code snippet because it took me forever to get right. I basically finished my entire code when I realized that I forgot that I needed to make sure that the user selects at least 1 character type. So this function starts the entire process in correct order, empties out the global array if the user is running the program after their first try. It uses a while loop to ensure that the user selects at least 1 character type before the program generates the password.
+I wanted to highlight this code snippet because storing user data is very inportant if you want certain features available to the user. I used the keyword "this". var taskText gets the value of the input field with class 'description' that is a sibling of the clicked button. var taskTime gets the text of the element with class 'hour' that is a sibling of the clicked button. then finally, the last line tells to store to local storage with key of taskTime and value of taskText.
 
 ```JavaScript
-
-
+function storeToLocal() {
+  $(".saveBtn").on("click", function () {
+    var taskText = $(this).siblings(".description").val();
+    var taskTime = $(this).siblings(".hour").text();
+    localStorage.setItem(taskTime, taskText);
+  });
+}
 ```
 
 ## Learning Points
 
-- Learned how to use functions in conjuction with else if statements, prompts, alerts
-- Learned the importance of ensuring functions are actually called and not just defined
-- Learned how to use empty variables to store user data that has been input
-- Learned how to generate a random index
-- Learned how to concatenate arrays
-- Learned how to user a for loop to iterate a desired number of times
-- Learned how to use while loops
+- Furthered knowledge of local storage, saving and retrieving
+- Learned how to use jQuery
+- Learned how to use bootstrap
+- Learned how to us day.js
+- Learned how to apply CSS classes when certain criteria are met
 
 ## Author Info
 
--- assests/script.js
+++ assests/script.js
@@ -1,104 +1,66 @@
 //display current day at top of the page
-function displayCurrentDateAtTop(){
+function displayCurrentDateAtTop() {
   $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY h:mm a"));
 }
 
 displayCurrentDateAtTop();
 
-//adding event listener so that when the save button is pressed the data is saved to locals and a default, past,present or future class is assigned to the current block
-  $(function() {
-    //select the element with the class .btn.saveBtn.col-2.col-md-1"
-    var saveButton = $(".btn.saveBtn.col-2.col-md-1");
-    saveButton.click(function() {
-     
-      var taskText = $(this).siblings(".description").val();
-      var taskTime = $(this).parent().attr("id");
+//adding event listener so that when the save button is pressed the data is saved to local
+$(function () {
+  var saveButton = $(".btn.saveBtn.col-2.col-md-1");
+  saveButton.click(function () {
+    var taskText = $(this).siblings(".description").val();
+    var taskTime = $(this).parent().attr("id");
 
-      storeToLocal({
-        taskText: taskText,
-        taskTime: taskTime
-      });
-      
+    storeToLocal({
+      taskText: taskText,
+      taskTime: taskTime,
     });
   });
-///////////////////////////////////need to fists remove current class then you can add the new class
+});
 
-  $(function() {
-    var currentTime = dayjs();
-    var currentHour = currentTime.hour();
-    console.log(currentHour);
-    // loop through every hour of the day and check to see if that hour is past, present or future of current hour, then add appropriate class to each box
-    for (var hour = 0; hour < 24; hour++) {
-      // var taskText = localStorage.getItem("hour-" + hour);
-      
-      //checks to see if there is string stored in block, if so the appropiate class will be applied
-      
-
-        
-        //  if hour is in the past
-        if (currentHour > hour) {
-          $("#hour-" + hour).addClass("past");
-          
-        }
-        //  if hour is present time
-        else if (currentHour === hour) {
-          $("#hour-" + hour).addClass("present");
-          
-        }
-        //  if hour is in the future
-        else {
-          $("#hour-" + hour).addClass("future");
-          
-        }
-      }
+$(function () {
+  var currentTime = dayjs();
+  var currentHour = currentTime.hour();
+  
+  // loop through every hour of the day and check to see if that hour is past, present or future of current hour, then add appropriate class to each box
+  for (var hour = 0; hour <24 ; hour++) {
+    //  if hour is in the past
+    if (currentHour > hour) {
+      $("#hour-" + hour).addClass("past");
     }
-  );
-  
-  //toggling the classes based on past, present or future
-  
-  
+    //  if hour is present time
+    else if (currentHour === hour) {
+      $("#hour-" + hour).addClass("present");
+    }
+    //  if hour is in the future
+    else {
+      $("#hour-" + hour).addClass("future");
+    }
+  }
+});
+
 
 //save to local storage function
-  function storeToLocal(){
-    $(".saveBtn").on("click", function(){
-      var taskText = $(this).siblings(".description").val();
-      var taskTime = $(this).siblings(".hour").text();
-      localStorage.setItem(taskTime, taskText);
-      
-    });
-  }
-  //retrieve from local storage, goes through every hour looking for stored data and retrieves it if there. if there is a saved task then it sets the textarea elemnt to the saved value
-  function retrieveFromLocal() {
-    for (var hour = 0; hour < 24; hour++) {
-      var taskTime = $("#hour-" + hour + " .hour").text();
-      var savedTaskText = localStorage.getItem(taskTime);
-      if (savedTaskText !== null) {
-        $("#hour-" + hour + " .description").val(savedTaskText);
-     
-
-      
-      }
+function storeToLocal() {
+  $(".saveBtn").on("click", function () {
+    var taskText = $(this).siblings(".description").val();
+    var taskTime = $(this).siblings(".hour").text();
+    localStorage.setItem(taskTime, taskText);
+  });
+}
+//retrieve from local storage, goes through every hour looking for stored data and retrieves it if there. if there is a saved task then it sets the textarea elemnt to the saved value
+function retrieveFromLocal() {
+  for (var hour = 0; hour < 24; hour++) {
+    var taskTime = $("#hour-" + hour + " .hour").text();
+    var savedTaskText = localStorage.getItem(taskTime);
+    if (savedTaskText !== null) {
+      $("#hour-" + hour + " .description").val(savedTaskText);
     }
   }
+}
 
-  retrieveFromLocal();
-
-
-
-  // col-8 input-field" id="text01"><
-
-
-
-
-
-
-
-
-
-
-
-
-
+retrieveFromLocal();
 
 
 
